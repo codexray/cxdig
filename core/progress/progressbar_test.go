218dcaa5f06b3417242f5f4ac468cbd00ee2a920
Olivier Montes <oliviermontes34@gmail.com>
2018-03-15 11:53:30 +0100

    Add unit tests for progress package

--- core/progress/progressbar_test.go
+++ core/progress/progressbar_test.go
@@ -1,1 +1,21 @@
 package progress
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBar(t *testing.T) {
+	p := ProgressBar{}
+	p.Init(10)
+	p.isCancelled = true
+	assert.Equal(t, true, p.IsCancelled())
+	p.isCancelled = false
+	assert.Equal(t, false, p.IsCancelled())
+	for i := 1; i <= 10; i++ {
+		p.Increment()
+		assert.Equal(t, i, p.Impl.Current())
+	}
+	p.Done()
+}
